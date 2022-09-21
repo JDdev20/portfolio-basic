77b4c57b41ba5049512211b3f3f6e2c0d1cd9286
JDdev20 <devjesusgil@gmail.com>
2022-09-21 06:40:18 -0430

    applying interactivity with JavaScript

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,24 @@
-                                /* This is my scripts */
+/* This is my scripts */
+
+const toggleTheme = document.getElementById("toggle-theme");
+const toggleIcon = document.getElementById("toggle-icon");
+const toggleText = document.getElementById("toggle-text");
+
+const toggleColors = document.getElementById("toggle-colors");
+
+const rootStyles = document.documentElement.style;
+
+toggleTheme.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+  if (toggleIcon.src.includes("dark-mode.svg")) {
+    toggleIcon.src = "assets/icons/light-mode.svg";
+    toggleText.textContent = "Light Mode";
+  } else {
+    toggleIcon.src = "assets/icons/dark-mode.svg";
+    toggleText.textContent = "Dark Mode";
+  }
+});
+
+toggleColors.addEventListener("click", (e) => {
+  rootStyles.setProperty("--primary-color", e.target.dataset.color);
+});
